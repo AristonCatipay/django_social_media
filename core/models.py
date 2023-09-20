a9eda1d1df4390e75510cb7fee22416d3e118697
AristonCatipay <aristoncatipay123@gmail.com>
2023-09-20 16:26:56 +0800

    Adding the signup functionality.

--- core/models.py
+++ core/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE),
-    id_user = models.IntegerField(),
-    bio = models.TextField(blank=True),
-    profile_image = models.ImageField(upload_to='profile_images', default='default_profile_image.jpg'),
-    location = models.CharField(max_length=100, blank=True),
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to='profile_images', default='default_profile_image.jpg')
+    location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
--- core/views.py
+++ core/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 from django.http import HttpResponse
+from . models import Profile
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'This email is already taken.')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'This username is already taken.')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                # Log user in and redirect the settings page.
+
+                # Create a Profile object for the new user.
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else: 
+            messages.info(request, 'Password don\'t match.')
+            return redirect('signup')
+    else: 
+        return render(request, 'signup.html')
