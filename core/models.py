aa368faf1eca121eab238393a493d274f6d74938
Ariston <aristoncatipay123@gmail.com>
2023-10-13 00:26:36 +0800

    Display the profile image of the person who post the image.

--- core/models.py
+++ core/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
+    profile = models.ForeignKey(Profile, related_name='profile_id_post', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user
@@ -29,6 +30,7 @@ class Post(models.Model):
 class LikePost(models.Model):
     post_id = models.CharField(max_length=500)
     username = models.CharField(max_length=100)
+    profile = models.ForeignKey(Profile, related_name='profile_id_like_post', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.username
