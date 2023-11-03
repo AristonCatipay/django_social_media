b5523b2e4db002ca0d7c2f85166d04cc5af39aa8
Ariston <aristoncatipay123@gmail.com>
2023-11-04 00:28:45 +0800

    Created the models for messenger app.

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Metadata(models.Model):
+    members = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateField(auto_now_add=True)
+    modified_at  = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-modified_at',)
+
+class Message(models.Model):
+    metadata = models.ForeignKey(Metadata, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='created_messages', on_delete=models.CASCADE)
