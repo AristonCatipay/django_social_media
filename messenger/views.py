92f08de8ef9bf5df01053cede6942129ed3e7182
Ariston <aristoncatipay123@gmail.com>
2023-11-04 19:58:44 +0800

    Display the users in the right panel of the message index page.

--- messenger/views.py
+++ messenger/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 def index(request):
+    users = User.objects.filter(is_staff=False, is_superuser=False)
+
     return render(request, 'messenger/index.html', {
         'title': 'Messenger',
+        'users': users,
     })
