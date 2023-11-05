79f7d1c0686e0e3dbd7109dda5e3513709703a43
Ariston <aristoncatipay123@gmail.com>
2023-11-05 22:14:26 +0800

    Create the messages page. This will display all the messages. To achieve this, added a related name to metadata in the connection of the messages and metadata model.

--- messenger/models.py
+++ messenger/models.py
@@ -11,7 +11,7 @@ class Metadata(models.Model):
         ordering = ('-modified_at',)
 
 class Message(models.Model):
-    metadata = models.ForeignKey(Metadata, on_delete=models.CASCADE)
+    metadata = models.ForeignKey(Metadata, related_name='metadata',on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='created_messages', on_delete=models.CASCADE)
--- messenger/urls.py
+++ messenger/urls.py
@@ -5,5 +5,6 @@ app_name = 'messenger'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:metadata_primary_key>/', views.messages, name='messages'),
     path('<int:searched_user_primary_key>/', views.add_message_or_redirect_to_messages, name='add_message_or_redirect_to_messages'),
 ]
