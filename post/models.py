a374dc29a8b81ab1d0f041707bb3043a68d9e457
Ariston <aristoncatipay123@gmail.com>
2024-01-11 19:07:48 +0800

    Create the Post model in the post app.

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Post(models.Model):
+    caption = models.TextField()
+    image = models.ImageField(upload_to='post_images')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.created_by.username
