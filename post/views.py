e6f73f56e33ace102cbe8359bfac573daca8f6ff
Ariston <aristoncatipay123@gmail.com>
2024-01-12 19:13:06 +0800

    Recreate the display post and user follow suggestion functionality in the post app.

--- post/views.py
+++ post/views.py
@@ -7,8 +7,26 @@ from itertools import chain
 
 @login_required(login_url='signin')
 def index(request):
+    # Get the list of usernames the current user is following
+    user_following = Followers.objects.filter(follower=request.user).values_list('leader_username', flat=True)
+
+    # Use the list of usernames to retrieve posts
+    user_following_feed = Post.objects.filter(user__in=user_following)
+
+    # Get a queryset of users that the current user is not following and is not the current user
+    suggestion_users = User.objects.exclude(username__in=user_following).exclude(username=request.user.username).order_by('?')
+
+    # Get the profiles of the suggested users
+    suggestion_profiles = Profile.objects.filter(user__in=suggestion_users)
+    
+    # Get all the liked posts
+    like_post_all = LikePost.objects.all()
+
     return render(request, 'index.html', {
         'title': 'Home',
+        'posts': user_following_feed,
+        'like_post': like_post_all,
+        'suggestion_profiles': suggestion_profiles[:3],  # Limit to the first 3 suggested profiles
     })
 
 @login_required(login_url='signin')
