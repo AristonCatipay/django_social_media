a9442e99d695db4f30c8bd7127bdde283aed1e7c
Ariston <aristoncatipay123@gmail.com>
2024-01-12 18:55:59 +0800

    Comment out all the newly created database models.

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index/', views.index, name='index'),
+    # path('index/', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('logout/', views.logout, name='logout'),
