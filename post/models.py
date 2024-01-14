fdebd211fd159efde1606db1458a3a6e0c29bfb4
Ariston <aristoncatipay123@gmail.com>
2024-01-14 23:53:45 +0800

    Update the post model.

--- post/models.py
+++ post/models.py
@@ -4,14 +4,15 @@ from datetime import datetime
 from user_profile.models import Profile
 import uuid
 
-# class Post(models.Model):
-#     caption = models.TextField()
-#     image = models.ImageField(upload_to='post_images')
-#     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(default=datetime.now)
+class Post(models.Model):
+    caption = models.TextField()
+    image = models.ImageField(upload_to='post_images')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now)
+    last_update = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return self.created_by.username
+    def __str__(self):
+        return self.created_by.username
     
 # class Like(models.Model):
 #     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -29,18 +30,18 @@ import uuid
 #     def __str__(self):
 #         return self.created_by.username
 
-class Post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_images')
-    caption = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now)
-    no_of_likes = models.IntegerField(default=0)
-    profile = models.ForeignKey(Profile, related_name='profile_id_post', on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+# class Post(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+#     user = models.CharField(max_length=100)
+#     image = models.ImageField(upload_to='post_images')
+#     caption = models.TextField()
+#     created_at = models.DateTimeField(default=datetime.now)
+#     no_of_likes = models.IntegerField(default=0)
+#     profile = models.ForeignKey(Profile, related_name='profile_id_post', on_delete=models.CASCADE)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.user
+#     def __str__(self):
+#         return self.user
     
 class LikePost(models.Model):
     username = models.CharField(max_length=100)
