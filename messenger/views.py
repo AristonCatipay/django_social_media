bd86941271ca43e5430454461e09c94c6e4fba18
Ariston <aristoncatipay123@gmail.com>
2024-01-15 22:15:32 +0800

    Delete the old models that have been changed.

--- messenger/views.py
+++ messenger/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from user_profile.models import Profile
 from .models import Metadata
 from .forms import MessageForm
 
