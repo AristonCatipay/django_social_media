ea6758157728ce1b478b7ebc715ae0d6f741c9ff
Ariston <aristoncatipay123@gmail.com>
2024-02-26 22:53:51 +0800

    Create the view region functionality.

--- address/urls.py
+++ address/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'address'
 
 urlpatterns = [
+    path('region/', views.view_region, name='view_region'),
     path('region/create/', views.create_region, name='create_region'),
     path('region/update/<int:primary_key>/', views.update_region, name='update_region'),
     path('province/create/', views.create_province, name='create_province'),
--- address/views.py
+++ address/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .forms import RegionForm, ProvinceForm, CityMunicipalityForm, BarangayForm
 from .models import Region, Province, City_Municipality, Barangay
+
+@login_required
+def view_region(request):
+    query = request.GET.get('query', '')
+    regions = Region.objects.all()
+
+    if query:
+        regions = regions.filter(Q(name__icontains=query) | Q(region_code__icontains=query) | Q(psgc_code__icontains=query))
+    return render(request, 'address/region.html', {
+        'title': 'Region',
+        'regions': regions,
+    })
 
 @login_required
 def create_region(request):
